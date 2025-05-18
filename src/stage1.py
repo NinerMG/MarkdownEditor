48b7c0a49d720c028e353c1ee64455e2c3569ef9
NinerMG <maciej.gala@hotmail.com>
2025-05-18 21:22:36 +0200

    stage 1 - simple print and test

---
+++ src/stage1.py
@@ -0,0 +1,18 @@
+snippet = """# The Beatles Favorite Songs
+
+One may say that they were the most famous **rock band** ever.
+The legendary **line-up** comprised the following people:
+* *John Lennon*
+* *Paul McCartney*
+* *George Harrison*
+* *Ringo Starr*
+
+Their most iconic songs include:
+* ***Hey Jude***
+* ***Let It Be***
+* ***Yesterday***
+* ***Come Together***
+* ~~Ob-La-Di, Ob-La-Da~~
+"""
+
+print(snippet)
